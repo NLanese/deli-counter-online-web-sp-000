33950a68d8a6a5257066e1140776856da1e972e2
Nick Lanese <nlanese21@gmail.com>
2020-06-13 23:55:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+katz_deli = []
+
+def line (katz_deli)
+  i = 0
+  len = katz_deli.length - 1
+  if (len < 0 )
+    puts "The line is currently empty."
+    return
+  else
+    print ("The line is currently:")
+    until (i > len)
+      print " #{i + 1}. #{katz_deli[i]}"
+      i = i + 1
+    end
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  position = katz_deli.find_index(name) + 1
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+def now_serving(katz_deli)
+  if (katz_deli.length == 0)
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
